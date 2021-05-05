4945cf79911660fcace2056098a6e343a0980875
tamakiii <tamakiii@users.noreply.github.com>
2021-05-06 01:46:44 +0900

    docker on docker w/ ubuntu

--- docker.mk
+++ docker.mk
@@ -12,7 +12,7 @@ build-ubuntu: ubuntu.docker
 	docker build -t $(IMAGE_UBUNTU) -f $< .
 
 login-ubuntu:
-	docker run -it --rm -v $(realpath .):/local/docker-scratch -w /local/docker-scratch $(IMAGE_UBUNTU) bash
+	docker run -it --rm --privileged -v $(realpath .):/local/docker-scratch -w /local/docker-scratch $(IMAGE_UBUNTU) bash
 
 clean:
 	docker image rm $(UBUNTU)
